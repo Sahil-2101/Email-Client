a5ec814fb52c28514a3d67f6878fa6439daa96dc
Sahil Khatri <158370644+Sahil-2101@users.noreply.github.com>
2024-08-22 00:19:21 +1000

    Add retrieve.h

---
+++ retrieve.h
@@ -0,0 +1,15 @@
+#ifndef RETRIEVE_H
+#define RETRIEVE_H
+#include "main.h"
+
+/**
+ * @brief To retrieve and to read from 
+ * the email that has been fetched and 
+ * then to remove any unwanted lines 
+ * then print the rest of the email body 
+ *
+ * 
+ */
+
+int retrieve_email(int sockfd, int messageNum);
+#endif
